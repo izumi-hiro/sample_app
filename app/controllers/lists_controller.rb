7ef5f789e551df1125c85a2124ff49e8dfcee64b
izumi-hiro <transformers2728frost@gmail.com>
2022-04-08 12:34:14 +0000

    [Start] 3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,9 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
   
+  def destroy
+    
+  
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
