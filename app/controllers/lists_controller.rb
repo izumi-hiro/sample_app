89e32b82d04e70c408dc683ed70bf60889f670e7
izumi-hiro <transformers2728frost@gmail.com>
2022-04-08 12:39:44 +0000

    [Add] 3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -28,7 +28,10 @@ class ListsController < ApplicationController
   end
   
   def destroy
-    
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
   
   private
   def list_params
